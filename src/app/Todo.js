242cc115c635b44e6a2177c91f4b469b40a802db
adidarali <alid.didarali@keytree.co.uk>
2018-07-16 15:35:10 +0100

    Todo app
    learing react by making small application

--- src/app/Todo.js
+++ src/app/Todo.js
@@ -1,9 +1,40 @@
 import React, { Component } from 'react';
 
 export class Todo extends Component {
+  constructor() {
+    super();                  // because we are extending component here
+    this.state = {
+      todos: [],               // actually we can store our todo here  
+      newTodo: ''
+    }
+  }
+    handleChange(e) {            // handling data for text input and store the data 
+      const {value} = e.target; 
+      this.setState({newTodo: value});
+    }
+    handleClick(e) {
+      e.preventDefault();
+      const todos = [...this.state.todos, this.input.value]
+      this.input.value = '';
+      this.setState({todos});
+    }
   render() {
-    return (
-      <div>hello</div>
+    return(
+      <div>
+        <form>
+        <input
+        // onChange={this.handleChange.bind(this)} value={this.state.newTodo}
+        ref={node => this.input = node}     // Instead of handling the state for the new todo, references the input field direct
+        type='text' placeholder='new todo'/>
+        <button onClick={this.handleClick.bind(this)}>create</button>
+        {/* <button onClick={this.handleClick.bind(this)}>click</button>
+        {this.state.nums.map(num => (<h3 key={num}>{num}</h3>))} */}
+        </form>
+        <ul> 
+        {/* todo for each item                      */}
+          {this.state.todos.map(todo => <li key={todo.toString()}>{todo}</li>)}           
+        </ul>
+      </div>
     );
   }
 }
